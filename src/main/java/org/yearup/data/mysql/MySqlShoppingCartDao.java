e21bed8f7316c6aaf33aa901b2aff8ae3d49fef9
HasnainArifYU <harif@pgh.yearup.org>
2024-06-23 12:55:34 -0400

    get cart by userid

--- src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
@@ -1,4 +1,49 @@
 package org.yearup.data.mysql;
 
-public class MySqlShoppingCartDao {
+import org.springframework.stereotype.Component;
+import org.yearup.data.ShoppingCartDao;
+import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class MySqlShoppingCartDao extends MySqlDaoBase implements ShoppingCartDao {
+
+    public MySqlShoppingCartDao(DataSource dataSource) {
+        super(dataSource);
+    }
+
+    @Override
+    public ShoppingCart getCartByUserId(int userId) {
+        ShoppingCart cart = new ShoppingCart();
+        cart.setUserId(userId);
+
+        String sql = "SELECT * FROM shopping_cart_items WHERE user_id = ?";
+
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, userId);
+
+            ResultSet resultSet = statement.executeQuery();
+            List<ShoppingCartItem> items = new ArrayList<>();
+            while (resultSet.next()) {
+                ShoppingCartItem item = mapRow(resultSet);
+                items.add(item);
+            }
+            cart.setItems(items);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return cart;
+    }
+
+
 }
